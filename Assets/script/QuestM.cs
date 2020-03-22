ae1f3e4d35eabd6828cc7ffa0bff7d33102c90ed
Rick708 <Rick708@users.noreply.github.com>
2020-03-23 08:19:07 +0900

    quest

---
+++ Assets/script/QuestM.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//クエストの管理
+public class QuestM : MonoBehaviour
+{
+        int currentStage = 0;  //現在のステージ進行度を取得
+
+
+    // Nextbutton押されたら起動
+    public void OnNextButton()
+    {
+       currentStage++;　//進行度を増加
+       Debug.Log("進行度増加"+currentStage);
+    }
+}
